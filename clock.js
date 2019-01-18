66ae0e5aee08974d2cb1f5ae3467f17939154fbd
Keegan Kelly <keegan.kelly@scientificgames.com>
2019-01-18 17:55:10 -0600

    Trying to figure out how I can set a time that can be used as an alarm.

--- clock.js
+++ clock.js
@@ -1,4 +1,5 @@
-var moment  = require("moment")
+var fs = require("fs");
+var moment  = require("moment");
 
 console.log(
   "===========================" + "\n" +
@@ -6,3 +7,15 @@ console.log(
   "Today's date is " + moment().format("MM-DD-YYYY") + "\n" +
   "==========================="
 )
+
+var time = moment(process.argv[2]);
+ console.log(typeof time);
+
+function setAlarm(time) {
+ fs.writeFile("alarm.js".time, (err) => {
+   if (err) throw err;
+   console.log("Alarm has been set.");
+ });
+}
+
+setAlarm(time);
